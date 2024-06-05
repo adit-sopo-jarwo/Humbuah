51af8815fb1cc716a3426bca65411fd0503e3bed
adit-sopo-jarwo <muhamadaditya1191@gmail.com>
2024-06-05 20:13:38 +0700

    refactor(tailwind.js): update components

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ export default {
     "./resources/**/*.js",
     "./resources/**/*.vue",
     'node_modules/preline/dist/*.js',
+    "./components/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {},
